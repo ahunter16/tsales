1111c8b1c2ecec60793e7d1eef65ef45bfc34053
ahunter16 <alex.hunter14@btinternet.com>
2014-07-21 10:56:49 +0100

    Fill Values Javascript

--- test_view/form.html.php
+++ test_view/form.html.php
@@ -10,7 +10,9 @@
 	<img src="http://www.converged.co.uk/images/page/converged-logo.gif" alt="Image not found">	<br>
 	<hr>
 	<body>
-
+<!-- 		<iframe src = "../base_values">
+			<p> Unsupported</p>
+		</iframe> -->
 		<form action = "" method = "post" id = "inputform">
 			<?php basevals(); ?>
 			<br><br>
@@ -19,7 +21,7 @@
 
 				<?php tablegenerate();?>
 			</table>
-			<label class = "martable" >Fill all Price Cells With: <input type = "text" ></label> 
+			<label class = "martable" >Fill all Price Cells With: <input id = "fillcells" type = "text" onblur = "pricefill()"></label> 
 			<br>
 			<input class = "martable" type = "submit" Value = "Calculate">
 			<input class = "martable" type = "button" value = "Clear" onclick = "clearinput()">
@@ -29,19 +31,44 @@
 		</form>
 		<br>
 	 	<br>
-	 	<label class = "martable"><strong>Calculated Prices Using Active Base Values (Default):</strong></label>
+	 	<label class = "martable0t"><strong>Prices Using Active Base Values (Default):</strong></label><br>
 	 	<br>
 		<?php global $quotearray;
 		global $testarray;
 		$x = 0;
 		table_populate($quotearray, $x); 
-		echo "<br>";
+		echo '<label class = "martable1t"><strong>Prices Using Test Base Values:</strong></label><br><br>';
 		//print_r($testarray);
 		$x = 1;
 		table_populate($testarray, $x);?>
 		
-		<br><br><br>
+		
 		<script>
+
+		function pricefill()
+		{
+			var fillval = document.getElementById("fillcells").value;
+			var suppliers = ["ttbann", "ttb1yr", "ttb3yr", "wayann", "way1yr", "way3yr", "btsann", "btpann", "eadann", "eadins"];
+			var bandwidths = ["10","20","30","40","50","100"];
+			var bw = bandwidths.length;
+			var supp = suppliers.length;
+			if (fillval || fillval == 0)
+			{
+				for(var i = 0; i < bw; i++)
+				{
+					for (var j = 0; j < supp; j++)
+					{
+						var index = suppliers[j].concat(bandwidths[i]);
+						document.getElementById(index).value = fillval;
+						document.getElementById(index).value = fillval;
+					}
+					var btstot = "btstot".concat(bandwidths[i]);
+					var btptot = "btptot".concat(bandwidths[i]);
+					document.getElementById(btstot).innerHTML = 2*fillval;
+					document.getElementById(btptot).innerHTML = 2*fillval;
+				}
+			}
+		}
 		function formsub()
 		{
 			document.getElementById("inputform").submit();
@@ -72,6 +99,32 @@
 				y.innerHTML = (btpann + wayann);
 			}
 		
+		}
+
+		function cellhighlight(x)
+		{
+			//document.getElementById("btstot10").innerHTML = x.id;
+			if ((x.id).slice(-1) == 1)
+			{	
+				var cellid = (x.id).slice(0,-1) + 0;
+			}
+			if ((x.id).slice(-1) == 0)
+			{	
+				var cellid = (x.id).slice(0,-1) + 1;
+			}
+			document.getElementById(cellid).style.border="3px solid red";
+		}
+		function cellunhighlight(x)
+		{
+			if ((x.id).slice(-1) == 1)
+			{	
+				var cellid = (x.id).slice(0,-1) + 0;
+			}
+			if ((x.id).slice(-1) == 0)
+			{	
+				var cellid = (x.id).slice(0,-1) + 1;
+			}
+			document.getElementById(cellid).style.border="1px solid black";
 		}
 		</script>
 
